3c2a3e390ef39232e375a111dccec7f10b09221d
Robinsstudio <robin.dosanjos@akur8.com>
2022-02-17 13:19:10 +0100

    Parser

--- Client.java
+++ Client.java
@@ -2,22 +2,22 @@ import java.util.List;
 
 public class Client {
 
-  private List<String> likedIngredients;
-  private List<String> dislikedIngredients;
+    private List<String> likedIngredients;
+    private List<String> dislikedIngredients;
 
-  public Client(
-    List<String> likedIngredients,
-    List<String> dislikedIngredients
-  ) {
-    this.likedIngredients = likedIngredients;
-    this.dislikedIngredients = dislikedIngredients;
-  }
+    public Client(
+        List<String> likedIngredients,
+        List<String> dislikedIngredients
+    ) {
+        this.likedIngredients = likedIngredients;
+        this.dislikedIngredients = dislikedIngredients;
+    }
 
-  public List<String> getDislikedIngredients() {
-    return dislikedIngredients;
-  }
+    public List<String> getDislikedIngredients() {
+        return dislikedIngredients;
+    }
 
-  public List<String> getLikedIngredients() {
-    return likedIngredients;
-  }
+    public List<String> getLikedIngredients() {
+        return likedIngredients;
+    }
 }
---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int numberOfClients = Integer.parseInt(scanner.nextLine());
+        Client[] clients = new Client[numberOfClients];
+
+        for (int i = 0; i < numberOfClients; i++) {
+            List<String> likedIngredients = Arrays.asList(scanner.nextLine().split(" "));
+            List<String> dislikedIngredients = Arrays.asList(scanner.nextLine().split(" "));
+
+            likedIngredients = new ArrayList<>(likedIngredients.subList(1, likedIngredients.size()));
+            dislikedIngredients = new ArrayList<>(dislikedIngredients.subList(1, dislikedIngredients.size()));
+
+            clients[i] = new Client(likedIngredients, dislikedIngredients);
+        }
+
+        scanner.close();
+    }
+}
