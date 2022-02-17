d55ac5da6a245da8c8db0c3d637406e81158cab1
Robinsstudio <robin.dosanjos@akur8.com>
2022-02-17 13:05:50 +0100

    Client class

---
+++ Client.java
@@ -0,0 +1,23 @@
+import java.util.List;
+
+public class Client {
+
+  private List<String> likedIngredients;
+  private List<String> dislikedIngredients;
+
+  public Client(
+    List<String> likedIngredients,
+    List<String> dislikedIngredients
+  ) {
+    this.likedIngredients = likedIngredients;
+    this.dislikedIngredients = dislikedIngredients;
+  }
+
+  public List<String> getDislikedIngredients() {
+    return dislikedIngredients;
+  }
+
+  public List<String> getLikedIngredients() {
+    return likedIngredients;
+  }
+}
